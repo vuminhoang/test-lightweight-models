6cc4e85b22fd54c37b7c91c38958c408210a1360
Hoang Vu Minh <minhhv@dft.vn>
2025-05-08 16:34:32 +0700

    add comment

--- test_ocr.py
+++ test_ocr.py
@@ -1,8 +1,10 @@
 import os
 from rapidocr import RapidOCR
 
+config_path = os.path.join("models", "config_rapid_ocr.yml")
+
 def process_image_with_ocr(input_path, output_path=None):
-    engine = RapidOCR()
+    engine = RapidOCR(config_path)
     result = engine(input_path)
 
     if output_path is None:
@@ -18,9 +20,11 @@ def process_image_with_ocr(input_path, output_path=None):
 
 # Example usage
 if __name__ == "__main__":
-    img_path = os.path.join("media", "quotes.png")
-    out_path = os.path.join("media", "quotes_result.png")
+    img_path = os.path.join("media", "test_ocr_eng.jpg")
+    out_path = os.path.join("media", "test_ocr_rapidocr.jpg")
 
     result = process_image_with_ocr(img_path, out_path)
 
     print(result)
+    print("----------------")
+    print(result.txts)
