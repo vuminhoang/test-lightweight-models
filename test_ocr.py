017110587016e4d39530b6823145f9b72357a007
Hoang Vu Minh <minhhv@dft.vn>
2025-05-08 16:37:12 +0700

    add config path

--- test_ocr.py
+++ test_ocr.py
@@ -1,6 +1,7 @@
 import os
 from rapidocr import RapidOCR
 
+# new config file, change recognition model to "en"
 config_path = os.path.join("models", "config_rapid_ocr.yml")
 
 def process_image_with_ocr(input_path, output_path=None):
