eda2a0c76b78d5f875ccfa9baafd03b9ab5453fd
Hoang Vu Minh <minhhv@dft.vn>
2025-05-12 14:36:34 +0700

    offline whisper

--- test_whisper.py
+++ test_whisper.py
@@ -3,12 +3,12 @@ from faster_whisper import WhisperModel
 from pathlib import Path
 
 
-def transcribe_audio(audio_file: str, model_size: str = "tiny.en", device: str = "cpu",
+def transcribe_audio(audio_file: str, model_path: str, device: str = "cpu",
                      compute_type: str = "float32") -> str:
 
     start_time = time.time()
 
-    model = WhisperModel(model_size, device=device, compute_type=compute_type)
+    model = WhisperModel(model_path, device=device, compute_type=compute_type)
     segments, _ = model.transcribe(audio_file)
 
     text = '\n'.join(segment.text for segment in segments)
@@ -18,11 +18,23 @@ def transcribe_audio(audio_file: str, model_size: str = "tiny.en", device: str =
 
     return text
 
+
 if __name__ == "__main__":
     base_dir = Path(__file__).resolve().parent
     audio_path = base_dir / 'media' / 'sing for the monent no hook 30s.mp3'
-    transcription = transcribe_audio(str(audio_path))
+
+    # Cần cd models -> git clone https://huggingface.co/guillaumekln/faster-whisper-tiny.en
+    # sau đó cd thư mục tiny.en, git lfs pull để kéo model.bin về
+    model_path = base_dir / 'models' / 'faster-whisper-tiny.en'
+
+    transcription = transcribe_audio(
+        str(audio_path),
+        model_path=str(model_path),
+        compute_type="int8"
+    )
+
     print(transcription)
+
 
 # Actual lyrics
 sing_for_the_moment = '''
